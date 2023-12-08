33f6cccabc35b6f783b916525213b3448d0e9a4c
Daris02 <hei.raymond.2@gmail.com>
2023-12-08 09:05:37 +0300

    feat: add transaction test

--- src/main/java/com/wallet/app/Main.java
+++ src/main/java/com/wallet/app/Main.java
@@ -2,8 +2,10 @@ package com.wallet.app;
 
 import com.wallet.app.model.Account;
 import com.wallet.app.model.Currency;
+import com.wallet.app.model.Transaction;
 import com.wallet.app.service.AccountService;
 import com.wallet.app.service.CurrencyService;
+import com.wallet.app.service.TransactionService;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +14,18 @@ public class Main {
 
     public static void walletTest() {
         CurrencyService curr = new CurrencyService();
-        System.out.println(curr.saveCurrency(new Currency("Dollar", "USD")));
-        System.out.println(curr.getAllCurrencies());
+        // System.out.println(curr.saveCurrency(new Currency("Dollar", "USD")));
+        // System.out.println(curr.getAllCurrencies());
 
         AccountService acc = new AccountService();
-        System.out.println(acc.saveAccount(new Account("saving account", "Bank")));
-        System.out.println(acc.getAllAccounts());
+        // System.out.println(acc.saveAccount(new Account("saving account", "Bank")));
+        // System.out.println(acc.getAllAccounts());
+
+        TransactionService tran = new TransactionService();
+        Transaction transaction1 = new Transaction("Gift", 20_000.0, "DEBIT", "059b0f00-fcf8-4b2c-bde7-4ec2d695f7f1");
+        Transaction transaction2 = new Transaction("Salary", 100_000.25, "CREDIT", "f790706d-24b1-4c5e-8825-7c6a6e71c102");
+        System.out.println(tran.saveTransaction(transaction1));
+        // System.out.println(tran.saveTransaction(transaction2));
+        // System.out.println(String.valueOf(transaction.getAmount()));
     }
 }
