3e5402e30743b51c47d9d2b93097bbd47bbc9db9
Daris02 <hei.raymond.2@gmail.com>
2023-12-08 14:16:17 +0300

    feat: add transaction list in account

--- src/main/java/com/wallet/app/model/Account.java
+++ src/main/java/com/wallet/app/model/Account.java
@@ -1,6 +1,7 @@
 package com.wallet.app.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import lombok.*;
 
@@ -16,9 +17,19 @@ public class Account {
     private Timestamp creationDate;
     private String type;
     private Integer currencyId;
+    private List<Transaction> transactionList;
         
     public Account(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public Account(String id, String name, Double balance, Timestamp creationDate, String type, Integer currencyId) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.creationDate = creationDate;
+        this.type = type;
+        this.currencyId = currencyId;
     }
 }
