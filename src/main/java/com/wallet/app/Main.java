cbc3cb11909275b932aedcabb1b54c82b80985a5
Daris02 <hei.raymond.2@gmail.com>
2023-12-08 18:27:13 +0300

    feat: add transfert test

--- src/main/java/com/wallet/app/Main.java
+++ src/main/java/com/wallet/app/Main.java
@@ -3,9 +3,12 @@ package com.wallet.app;
 import com.wallet.app.model.Account;
 import com.wallet.app.model.Currency;
 import com.wallet.app.model.Transaction;
+import com.wallet.app.model.Transfert;
 import com.wallet.app.service.AccountService;
 import com.wallet.app.service.CurrencyService;
 import com.wallet.app.service.TransactionService;
+import com.wallet.app.service.TransfertService;
+
 import java.time.LocalDateTime;
 
 public class Main {
@@ -29,16 +32,18 @@ public class Main {
         // System.out.println(acc.getAllAccounts());
 
         // System.out.println(acc.getBalancesHistory("a28301bf-593e-435e-9e9a-bdf7d00cb64a"));
-        System.out.println(acc.getBalancesHistoryWithDate("a28301bf-593e-435e-9e9a-bdf7d00cb64a", LocalDateTime.of(2023, 12, 8, 17, 0, 0), LocalDateTime.of(2023, 12, 8, 18, 0, 0)));
+        // System.out.println(acc.getBalancesHistoryWithDate("a28301bf-593e-435e-9e9a-bdf7d00cb64a", LocalDateTime.of(2023, 12, 8, 17, 0, 0), LocalDateTime.of(2023, 12, 8, 18, 0, 0)));
 
 
         TransactionService tran = new TransactionService();
         Transaction transaction1 = new Transaction("Salary", 100_000.0, "CREDIT", "a28301bf-593e-435e-9e9a-bdf7d00cb64a");
-        Transaction transaction2 = new Transaction("Gift", 2_000_000.0, "CREDIT", "78047217-9fb5-449c-9622-57fc960bdeeb");
+        Transaction transaction2 = new Transaction("Gift", 2_000_000.0, "CREDIT", "a216cda3-8387-4a51-9369-414e43c5c1bc");
 
         // System.out.println(tran.saveTransaction(transaction1));
         // System.out.println(tran.saveTransaction(transaction2));
 
-        // System.out.println(String.valueOf(transaction.getAmount()));
+        TransfertService traf = new TransfertService();
+        traf.saveTransfert("a28301bf-593e-435e-9e9a-bdf7d00cb64a", "a216cda3-8387-4a51-9369-414e43c5c1bc", 50_000.0);
+        System.out.println(traf.getAllTransferts());
     }
 }
