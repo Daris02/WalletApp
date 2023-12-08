50e59cc05f0587351d1501d9c9a3ff191525d783
Daris02 <hei.raymond.2@gmail.com>
2023-12-08 09:05:06 +0300

    fix: change output of transaction request

--- src/main/java/com/wallet/app/service/TransactionService.java
+++ src/main/java/com/wallet/app/service/TransactionService.java
@@ -1,12 +1,16 @@
 package com.wallet.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.wallet.app.model.Account;
 import com.wallet.app.model.Transaction;
+import com.wallet.app.repository.AccountRepository;
 import com.wallet.app.repository.TransactionRepository;
 
 public class TransactionService {
     private TransactionRepository transactionRepo = new TransactionRepository();
+    private AccountRepository accountRepo = new AccountRepository();
 
     public Transaction getTransactionById(String id) {
         return transactionRepo.getById(id);
@@ -16,11 +20,16 @@ public class TransactionService {
         return transactionRepo.findAll();
     }
 
-    public Transaction saveTransaction(Transaction Transaction) {
-        return transactionRepo.save(Transaction);
+    public Account saveTransaction(Transaction transaction) {
+        transactionRepo.save(transaction);
+        return accountRepo.getById(transaction.getAccountId());
     }
 
-    public List<Transaction> saveAllTransactions(List<Transaction> transactions) {
-        return transactionRepo.saveAll(transactions);
+    public List<Account> saveAllTransactions(List<Transaction> transactions) {
+        List<Account> accounts = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            accounts.add(saveTransaction(transaction));
+        }
+        return accounts;
     }
 }
