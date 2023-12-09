3251584cfbdd09cf8142ba505a263d3721bc963a
Daris02 <hei.raymond.2@gmail.com>
2023-12-09 18:22:31 +0300

    feat: add conditions to transfer

--- src/main/java/com/wallet/app/service/TransfertService.java
+++ src/main/java/com/wallet/app/service/TransfertService.java
@@ -10,16 +10,27 @@ import com.wallet.app.repository.TransfertRepository;
 public class TransfertService {
     TransactionRepository transactionRepository = new TransactionRepository();
     TransfertRepository transfertRepository = new TransfertRepository();
+    AccountService accountService = new AccountService();
 
     public List<Transfert> getAllTransferts() {
         return transfertRepository.findAll();
     }
     
     public Transfert saveTransfert(String debtorId, String creditorId, Double amount) {
+        if (debtorId == creditorId) {
+            System.out.println("Transfer failed: an account can't do transfer to himself.");
+            return null;
+        } else if (accountService.getAccountById(debtorId).getCurrency().getCode() != accountService.getAccountById(creditorId).getCurrency().getCode()) {
+            System.out.println("Transfer failed: an account can't do transfer to another account with different currency.");
+            return null;
+        }
+
         transactionRepository.save(new Transaction("Transfert", amount, "DEBIT", debtorId));
         transactionRepository.save(new Transaction("Transfert", amount, "CREDIT", creditorId));
+        
         Transfert transfert = new Transfert(debtorId, creditorId, amount);
         transfertRepository.save(transfert);
+        
         return transfert;
     }
     
