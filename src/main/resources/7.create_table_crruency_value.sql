884872a360f458d138a0eaca76bfcaf346e4ff04
Daris02 <hei.raymond.2@gmail.com>
2023-12-10 11:55:59 +0300

    fix: change column type and add default insert

--- src/main/resources/7.create_table_crruency_value.sql
+++ src/main/resources/7.create_table_crruency_value.sql
@@ -3,9 +3,12 @@ DROP TABLE IF EXISTS "currency_value";
 
 CREATE TABLE IF NOT EXISTS "currency_value" (
     id BIGSERIAL PRIMARY KEY,
-    currency_source VARCHAR(100) NOT NULL,
-    currency_destination VARCHAR(100) NOT NULL,
+    currency_source INT REFERENCES "currency"(id),
+    currency_destination INT REFERENCES "currency"(id),
     amount DECIMAL NOT NULL,
     date_effect DATE
 );
 
+INSERT INTO "currency_value" (currency_source, currency_destination, amount, date_effect) VALUES
+(2, 1, 4_500, '2023-12-10'),
+(2, 1, 4_600, '2023-12-11');
