4f74d1c6b915c58aa0c0cfbc45f4d500a268a4bf
Daris02 <hei.raymond.2@gmail.com>
2023-12-18 10:13:35 +0300

    fix: update  basic test wallet

--- src/main/java/com/wallet/app/Main.java
+++ src/main/java/com/wallet/app/Main.java
@@ -1,5 +1,6 @@
 package com.wallet.app;
 
+import com.wallet.app.config.ConnectionDB;
 import com.wallet.app.model.Account;
 import com.wallet.app.model.Currency;
 import com.wallet.app.model.CurrencyValue;
@@ -7,6 +8,7 @@ import com.wallet.app.model.Transaction;
 import com.wallet.app.model.Transfert;
 import com.wallet.app.service.AccountService;
 import com.wallet.app.service.CurrencyService;
+import com.wallet.app.service.TransactionService;
 import com.wallet.app.service.TransfertService;
 
 import java.time.LocalDateTime;
@@ -22,28 +24,32 @@ public class Main {
             // System.out.println(curr.getAllCurrencies());
             // System.out.println(curr.getAllCurrencyValues());
             // System.out.println(curr.getCurrencyValueById("1"));
-            // System.out.println(curr.saveCurrencyValue(new CurrencyValue("2", "1", 4550.0)));
+            // System.out.println(curr.saveCurrencyValue(new CurrencyValue("2", "1", 4601.13)));
 
 
         AccountService acc = new AccountService();
         Account C1 = new Account("test account", "Cash");
         Account C2 = new Account("current account", "Bank");
+            String id1 = "f37c2443-7225-436d-ba3f-1abc80a2fa9d";
+            String id2 = "701cb97d-e850-4091-af57-bdb7659c2da6";
+
             // System.out.println(acc.saveAccount(C1, "MGA"));
             // System.out.println(acc.saveAccount(C2, "EUR"));
 
-            // System.out.println(acc.getAccountById("701cb97d-e850-4091-af57-bdb7659c2da6"));
+            // System.out.println(acc.getAccountById(id2));
             // System.out.println(acc.getAllAccounts());
 
-            // System.out.println(acc.getBalancesHistory("f37c2443-7225-436d-ba3f-1abc80a2fa9d"));
-            // System.out.println(acc.getBalancesHistoryWithDate("f37c2443-7225-436d-ba3f-1abc80a2fa9d", LocalDateTime.of(2023, 12, 8, 18, 0, 0), LocalDateTime.of(2023, 12, 15, 18, 0, 0)));
+            // System.out.println(acc.getBalancesHistory(id1));
+            // System.out.println(acc.getBalancesHistoryWithDate(id1, LocalDateTime.of(2023, 12, 18, 8, 0, 0), LocalDateTime.of(2023, 12, 22, 8, 0, 0)));
+
 
 
         TransactionService tran = new TransactionService();
-        Transaction T1 = new Transaction("Salary", 10_000.0, "CREDIT", "f37c2443-7225-436d-ba3f-1abc80a2fa9d", 32);
-        Transaction T2 = new Transaction("Salary", 20.0, "CREDIT", "701cb97d-e850-4091-af57-bdb7659c2da6", 32);
-        Transaction T3 = new Transaction("Gift", 20_000.0, "DEBIT", "f37c2443-7225-436d-ba3f-1abc80a2fa9d", 3);
-        Transaction T4 = new Transaction("New shoes", 50_000.0, "DEBIT", "701cb97d-e850-4091-af57-bdb7659c2da6", 4);
-        Transaction T5 = new Transaction("Gift", 2000.0, "DEBIT", "f37c2443-7225-436d-ba3f-1abc80a2fa9d", 5);
+        Transaction T1 = new Transaction("Salary", 10_000.0, "CREDIT", id1, 32);
+        Transaction T2 = new Transaction("Salary", 20.0, "CREDIT", id2, 32);
+        Transaction T3 = new Transaction("Gift", 20_000.0, "DEBIT", id1, 3);
+        Transaction T4 = new Transaction("New shoes", 50_000.0, "DEBIT", id2, 4);
+        Transaction T5 = new Transaction("Gift", 2000.0, "DEBIT", id1, 5);
 
             // System.out.println(tran.saveTransaction(T1));
             // System.out.println(tran.saveTransaction(T2));
@@ -55,19 +61,23 @@ public class Main {
 
             // System.out.println(tran.getTransactionById("f00ff521-eae3-4a99-839c-3437fb7d1522"));
 
+
+
         TransfertService traf = new TransfertService();
-            traf.saveTransfert("701cb97d-e850-4091-af57-bdb7659c2da6", "f37c2443-7225-436d-ba3f-1abc80a2fa9d", 5.0, "median");
+            // traf.saveTransfert(id2, id1, 5.0, "median");
             // System.out.println(traf.getAllTransferts());
 
+
+
         // Spend Amount ---
-        LocalDateTime start =  LocalDateTime.of(2023, 12, 12, 18, 0, 0);
-        LocalDateTime end = LocalDateTime.of(2023, 12, 15, 18, 0, 0);
+        LocalDateTime start =  LocalDateTime.of(2023, 12, 18, 8, 0, 0);
+        LocalDateTime end = LocalDateTime.of(2023, 12, 22, 8, 0, 0);
             
             // --- SQL
-            System.out.println(acc.getAllTotalSpendAmounts("f37c2443-7225-436d-ba3f-1abc80a2fa9d", start, end));
+            // System.out.println(acc.getAllTotalSpendAmounts(id1, start, end));
             
             // --- Java Only
-            System.out.println(acc.findAllTotalSpendAmounts("f37c2443-7225-436d-ba3f-1abc80a2fa9d", start, end));
+            // System.out.println(acc.findAllTotalSpendAmounts(id1, start, end));
 
     }
 }
