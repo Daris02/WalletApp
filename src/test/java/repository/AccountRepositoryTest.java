620af20093f0af5574ebdc7f6a27341a6bad2ee7
Daris02 <hei.raymond.2@gmail.com>
2024-01-06 10:56:43 +0300

    feat: add test for save account

--- src/test/java/repository/AccountRepositoryTest.java
+++ src/test/java/repository/AccountRepositoryTest.java
@@ -1,16 +1,31 @@
 package repository;
 
-import com.wallet.app.model.Account;
-import com.wallet.app.repository.AccountRepository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 
+import com.wallet.app.model.Account;
+import com.wallet.app.model.Currency;
+import com.wallet.app.repository.AccountRepository;
+
+
 public class AccountRepositoryTest {
-    public AccountRepository accountRepository = new AccountRepository();
+    AccountRepository repo = new AccountRepository();
+
+    @Test
+    void save() {
+        LocalDateTime dateTime = LocalDateTime.of(2024, 1, 6, 10, 0, 0);
+        Account account = new Account("f028d677-18fb-4e00-af96-e482e54236a1", "Account 1", 0.0, Timestamp.valueOf(dateTime), "Bank", new Currency("1", "Ariary", "MGA"), null);
+        // repo.save(account);
+        // assertEquals(account.getName(), repo.getById("f028d677-18fb-4e00-af96-e482e54236a1").getName());
+    }
     
     @Test
-    void get_account_by_id() {
-        Account accountTest = new Account();
-        assertEquals(accountTest.getClass(), accountRepository.getById("f37c2443-7225-436d-ba3f-1abc80a2fa9d").getClass());
+    void getById() {
+        Account account = new Account();
+        assertEquals(account.getClass(), repo.getById("f028d677-18fb-4e00-af96-e482e54236a1").getClass());
     }
 }
