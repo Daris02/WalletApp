30580a4cbf609b78c9797dd0a7d30c8929244cb6
Daris02 <hei.raymond.2@gmail.com>
2024-01-13 19:47:47 +0300

    fix: update manuel test

--- src/main/java/com/wallet/app/Main.java
+++ src/main/java/com/wallet/app/Main.java
@@ -20,22 +20,25 @@ public class Main {
 
     public static void walletTest() {
         CurrencyService curr = new CurrencyService();
-            // System.out.println(curr.saveCurrency(new Currency("Dollar", "USD")));
+            // System.out.println(curr.saveCurrency(new Currency("Ariary", "MGA")));
+            // System.out.println(curr.saveCurrency(new Currency("Euro", "EUR")));
             // System.out.println(curr.getAllCurrencies());
+            // System.out.println(curr.getCurrencyById(1));
             // System.out.println(curr.getAllCurrencyValues());
-            // System.out.println(curr.getCurrencyValueById("1"));
+            // System.out.println(curr.getCurrencyValueById(0));
             // System.out.println(curr.saveCurrencyValue(new CurrencyValue("2", "1", 4601.13)));
 
 
         AccountService acc = new AccountService();
-        Account C1 = new Account("test account", "Cash");
-        Account C2 = new Account("current account", "Bank");
-            String id1 = "f37c2443-7225-436d-ba3f-1abc80a2fa9d";
-            String id2 = "701cb97d-e850-4091-af57-bdb7659c2da6";
+        Account C1 = new Account("AccountMGA_1", "Cash", 1);
+        Account C2 = new Account("AccountEUR_2", "Bank", 2);
+            String id1 = "a5868f16-2b96-4773-8b17-163d26b1e42a";
+            String id2 = "746faf84-b7de-477b-a2b8-4b9fdcd79d27";
 
-            // System.out.println(acc.saveAccount(C1, "MGA"));
-            // System.out.println(acc.saveAccount(C2, "EUR"));
+            // System.out.println(acc.saveAccount(C1));
+            // System.out.println(acc.saveAccount(C2));
 
+            // System.out.println(acc.getAccountById(id1));
             // System.out.println(acc.getAccountById(id2));
             // System.out.println(acc.getAllAccounts());
 
@@ -45,8 +48,8 @@ public class Main {
 
 
         TransactionService tran = new TransactionService();
-        Transaction T1 = new Transaction("Salary", 10_000.0, "CREDIT", id1, 32);
-        Transaction T2 = new Transaction("Salary", 20.0, "CREDIT", id2, 32);
+        Transaction T1 = new Transaction("Salary", 10_000.0, "CREDIT", id1, 75);
+        Transaction T2 = new Transaction("Salary", 20.0, "CREDIT", id2, 75);
         Transaction T3 = new Transaction("Gift", 20_000.0, "DEBIT", id1, 3);
         Transaction T4 = new Transaction("New shoes", 50_000.0, "DEBIT", id2, 4);
         Transaction T5 = new Transaction("Gift", 2000.0, "DEBIT", id1, 5);
@@ -57,7 +60,7 @@ public class Main {
             // System.out.println(tran.saveTransaction(T4));
             // System.out.println(tran.saveTransaction(T5));
 
-            // System.out.println(tran.getTransactionById("aa9404fd-aa17-4754-89a6-4827495ca000"));
+            // System.out.println(tran.getTransactionById("c5f219d0-72f8-4bbd-ac5a-09b16672ecec"));
 
         TransfertService traf = new TransfertService();
             // System.out.println(traf.saveTransfert(id2, id1, 5.0, "median"));
@@ -66,14 +69,14 @@ public class Main {
 
 
         // Spend Amount ---
-        LocalDateTime start =  LocalDateTime.of(2024, 1, 3, 8, 0, 0);
-        LocalDateTime end = LocalDateTime.of(2024, 1, 4, 8, 0, 0);
+        LocalDateTime start =  LocalDateTime.of(2024, 1, 13, 8, 0, 0);
+        LocalDateTime end = LocalDateTime.of(2024, 1, 14, 8, 0, 0);
             
             // --- SQL
-            // System.out.println(acc.getAllTotalSpendAmounts(id1, start, end));
+            System.out.println(acc.getAllTotalSpendAmounts(id1, start, end));
             
             // --- Java Only
-            // System.out.println(acc.findAllTotalSpendAmounts(id1, start, end));
+            System.out.println(acc.findAllTotalSpendAmounts(id1, start, end));
 
     }
 }
